2b0e96e9be5edd19b3cd0aefc6cfa10f18988f4e
tansudasli <tansudasli@gmail.com>
2025-01-15 16:17:58 +0300

    restructuring

--- mac-init.sh
+++ mac-init.sh
@@ -1,3 +1,6 @@
+# 3 main categories [OS | Coding | Virtualization] 
+#
+#
 # homebrew: package manager
 # manual installation for [virtualbox, virtualbox-extension-pack, sdkman, sketch 91.4, sketch-toolbox ...]
 # use npm package manager for js things [ts, angular, firebase ...]
@@ -9,8 +12,7 @@ xcode-select --install # mac development cli: clang etc...
 brew install curl
 brew install wget
 brew install watch     # watch -n 0.2 curl .... to run continuously
-brew install aria2     # torrent download cli
-#aria2c -j 3 -s 3 --listen-port=6951-6970 -T ubuntu-24.10-desktop-amd64.iso.torrent
+brew install aria2     # torrent download cli => aria2c -j 3 -s 3 --listen-port=6951-6970 -T ubuntu-24.10-desktop-amd64.iso.torrent
 brew install --cask google-chrome
 brew install --cask whatsapp
 
@@ -28,6 +30,27 @@ wget -O Downloads/dimmedmonokai https://raw.githubusercontent.com/mbadolato/iTer
 wget -O Downloads/dracula+ https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/schemes/Dracula+.itermcolors
 
 source .zshrc
+
+# ansible
+# use ~/.ansible as home
+# create seperate inventory file and set in ansible command always
+# use service account private key
+brew install ansible
+
+#cloud provider CLIs
+# > create gcp account
+#run gcloud init to configure gcp, default project/region/zone
+#run as many as u want, if u need to add more accounts! .)
+brew install --cask google-cloud-sdk
+
+gcloud init 
+
+# ****** coding ***************
+
+# create your coding directory. 
+# download from repository by git clone ...
+mkdir ~/coding
+mkdir ~/Downloads/ISO
 
 # IDEs: vscode & intellij
 # brew install --cask jetbrains-toolbox [2023.x license]
@@ -74,47 +97,6 @@ brew install go
 brew install gcc      # c,c++ compiler
 brew install cmake    # build manager for C, C++
 
-# create your coding directory. 
-# download from repository by git clone ...
-mkdir ~/coding
-mkdir ~/Downloads/ISO
-
-
-# virtualization
-
-# virtualbox
-# >> download virtualbox from virtualbox.org
-# >> download virtualbox extension pack and configure in virtualbox app.
-
-# docker
-# > create docker hub account
-# > add helm repo for chart repos
-brew install --cask docker     #docker desktop & cli
-
-docker login
-
-# k8s
-# > configure kubectl for a remote k8 cluster (i.e k8s cluster on gcp). `gcloud container clusters get-credentials $CLUSTER_NAME` will do the job,
-#brew install kubectl           #kubectl commands
-#brew install kompose           #convert docker-compose.yaml to k8s   
-#brew install helm              #k8 package manager + cluster manager
-
-#helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-
-
-# below is just a hint. runing kubernetes or minicube is not meaningful on local!!
-# so u need to define remote kubernetes engine
-# Read if you need ---> configure GKE cluster to access from your local machine: https://kubernetes.io/docs/tasks/tools/install-kubectl/"
-
-
-#cloud provider CLIs
-# > create gcp account
-#run gcloud init to configure gcp, default project/region/zone
-#run as many as u want, if u need to add more accounts! .)
-brew install --cask google-cloud-sdk
-
-gcloud init 
-
 
 # nodejs
 # npm: package manager for js packages
@@ -149,14 +131,36 @@ brew install --cask skyfonts
 # READ -> https://support.google.com/materialgallery/answer/9270373?hl=en
 # curl -O https://material.io/tools/theme-editor/downloads/production/material.sketchplugin.zip
 
-# ansible
-# use ~/.ansible as home
-# create seperate inventory file and set in ansible command always
-# use service account private key
-brew install ansible
-
 
 # ethereum & blockchain
 npm install -g truffle
 npm install -g ganache-cli
+
+# ****** virtualization *******
+
+# virtualbox
+# >> download virtualbox from virtualbox.org
+# >> download virtualbox extension pack and configure in virtualbox app.
+
+# docker
+# > create docker hub account
+# > add helm repo for chart repos
+brew install --cask docker     #docker desktop & cli
+
+docker login
+
+# k8s
+# > configure kubectl for a remote k8 cluster (i.e k8s cluster on gcp). `gcloud container clusters get-credentials $CLUSTER_NAME` will do the job,
+#brew install kubectl           #kubectl commands
+#brew install kompose           #convert docker-compose.yaml to k8s   
+#brew install helm              #k8 package manager + cluster manager
+
+#helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+
+
+# below is just a hint. runing kubernetes or minicube is not meaningful on local!!
+# so u need to define remote kubernetes engine
+# Read if you need ---> configure GKE cluster to access from your local machine: https://kubernetes.io/docs/tasks/tools/install-kubectl/"
+
+
 
