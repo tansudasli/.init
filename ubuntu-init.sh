cfe387c121cce4a2de509151ad4eac8be5853c5d
tansudasli <tansudasli@gmail.com>
2025-02-04 23:57:42 +0300

    docker replaced w/ podman

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -192,18 +192,10 @@ sudo apt install libnss-libvirt -y
 #sudo multipass set local.driver=libvirt
 
 # docker
-# > create docker hub account
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# docker desktop ui: no need
+# docker cli: use podman
+sudo apt install podman
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update \
-     && docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
